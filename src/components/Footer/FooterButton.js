7778eac8b6707a587edfb383d34b1bda0bd5eb3a
Kirill <Lequeston@yandex.ru>
2020-05-12 04:10:32 +0300

    Подарок от доброго деда мороза Кирилла

--- src/components/Footer/FooterButton.js
+++ src/components/Footer/FooterButton.js
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 class FooterButton extends React.Component {
  render() {
    return (
-   <a className = {this.props.className} as={this.props.as} to={this.props.to}>{this.props.name}</a>
+   <Link
+     className = {this.props.className}
+     to={this.props.to}
+   >
+     {this.props.name}
+   </Link>
    );
  }
 }
